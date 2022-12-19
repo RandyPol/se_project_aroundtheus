304713d28d42c8e6e91b298f3af11f941e441de0
Ran Pol <rnd809@gmail.com>
2022-12-18 20:05:42 -0500

    Feat: Add card to container function

--- scripts/index.js
+++ scripts/index.js
@@ -25,9 +25,14 @@ const initialCards = [
   },
 ]
 
+// Cards container
+const cardsContainer = document.querySelector('.cards')
+// Card Template
+const cardTemplate = document.querySelector('#card').content
+
 // find the form fields in the DOM
-const nameInput = document.querySelector('.form__input[id="name"]')
-const roleInput = document.querySelector('.form__input[id="aboutMe"]')
+const nameInput = document.querySelector('#name')
+const roleInput = document.querySelector('#aboutMe')
 const profileName = document.querySelector('.profile__name')
 const profileRole = document.querySelector('.profile__role')
 
@@ -60,10 +65,6 @@ closeAllModal.forEach((closeButton) =>
   })
 )
 
-// find the form fields in the DOM
-const titleInput = document.querySelector('.form__input[id="title"]')
-const imageInput = document.querySelector('.form__input[id="imageLink"]')
-
 function handleProfileFormSubmit(event) {
   event.preventDefault()
   if (event.target.parentElement.id === 'modalEdit') {
@@ -72,7 +73,10 @@ function handleProfileFormSubmit(event) {
     closePopup(event.target.closest('.modal'))
     return
   }
-console.log("Klk wawawa")
+  // Adding New Card
+  const name = document.querySelector('#title').value
+  const link = document.querySelector('#imageLink').value
+  addCardElement({ name, link })
 }
 
 // Getting all form and adding the submit event listerner
@@ -81,28 +85,17 @@ formElement.forEach((form) =>
   form.addEventListener('submit', handleProfileFormSubmit)
 )
 
-// Cards container
-const cardsContainer = document.querySelector('.cards')
-
-// Card Template
-const cardTemplate = document.querySelector('#card').content
-
-function getCardElement(data) {
+// Add cards function
+function addCardElement(data) {
   let cardElement = cardTemplate.querySelector('.card').cloneNode(true)
-
   // Add Content
   const cardElementImage = cardElement.querySelector('.card__column-image')
   cardElementImage.src = data.link
   cardElementImage.alt = data.name
   cardElement.querySelector('.card__column-image-title').textContent = data.name
-
-  return cardElement
+  cardsContainer.append(cardElement)
 }
 
 initialCards.forEach((cardData) => {
-  let createCard = getCardElement(cardData)
-  cardsContainer.append(createCard)
+  addCardElement(cardData)
 })
-
-// Add new card
-const addCardButton = document.querySelector('.profile__add-button')
