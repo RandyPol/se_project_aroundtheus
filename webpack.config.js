d0efac3f2baad8e561839e10f0f78460f27635b3
Ran Pol <rnd809@gmail.com>
2023-01-13 14:55:38 -0500

    Update the webpack configuracion.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const path = require('path') // connect path to webpack config
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -6,33 +9,44 @@ module.exports = {
     main: './src/index.js',
   },
   output: {
-    // rewrite the output point using the path utility
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: '',
   },
-  mode: 'development',
+  target: ['web', 'es5'],
   stats: 'errors-only',
+  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // specifies a folder from where to serve the application and its contents
-    compress: true, // this will speed up file loading in development mode
-    port: 8080, // will open your site at localhost:8080 (you can use another port)
-    open: true, // site will open automatically in the browser after executing npm run dev
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true,
     liveReload: true,
     hot: false,
   },
   module: {
     rules: [
-      // this is an array of rules
-      // add an object containing rules for Babel to it
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: 'babel-loader',
-        // exclude the node_modules folder, we don't need to process files in it
         exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(), // connect the plugin for merging CSS files
+  ],
 }
