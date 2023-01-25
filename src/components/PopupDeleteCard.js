308347be7a5bf66551ed7cbe2b3de8c52ee9b868
Ran Pol <rnd809@gmail.com>
2023-01-24 22:47:04 -0500

    Feat: Create the Delete card model class extended by Popup

---
+++ src/components/PopupDeleteCard.js
@@ -0,0 +1,15 @@
+import Popup from './Popup.js'
+
+export default class PopupDeleteCard extends Popup {
+  constructor(popupSelector, handleCardDelete) {
+    super(popupSelector)
+    this._handleCardDelete = handleCardDelete
+  }
+
+  setEventListeners() {
+    super.setEventListeners()
+    this._popupElement
+      .querySelector('.modal__button-yes')
+      .addEventListener('click', this._handleCardDelete)
+  }
+}
