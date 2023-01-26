79b3629c7a5d24d9fd908eea813afe7003bb0355
Ran Pol <rnd809@gmail.com>
2023-01-25 23:48:21 -0500

    Create the api call to update the user avatar.

--- src/components/Api.js
+++ src/components/Api.js
@@ -123,4 +123,23 @@ export default class Api {
       return Promise.reject(`Error: ${res.status}`)
     })
   }
+
+  /**
+   *
+   * @param {*} data avatar info to be updated
+   * @retuns The updated avatar info from the server
+   */
+  patchUserAvatar(data) {
+    console.log(`In api call ${JSON.stringify(data, null, 2)}`)
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      return Promise.reject(`Error: ${res.status}`)
+    })
+  }
 }
