fdf4633c3a143da30cbaffd4165734d0623bfe9f
Ran Pol <rnd809@gmail.com>
2023-01-26 00:21:46 -0500

    Account for the avatar data in the UserInfo class.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,21 @@
 export default class UserInfo {
-  constructor({ nameSelector, jobSelector }) {
+  constructor({ nameSelector, jobSelector, avatarSelector }) {
     this._name = document.querySelector(nameSelector)
     this._about = document.querySelector(jobSelector)
+    this._avatar = document.querySelector(avatarSelector)
   }
 
   getUserInfo() {
-    return { name: this._name.textContent, about: this._about.textContent }
+    return {
+      name: this._name.textContent,
+      about: this._about.textContent,
+      avatar: this._avatar.src,
+    }
   }
 
-  setUserInfo({ name, about }) {
+  setUserInfo({ name, about, avatar }) {
     this._name.textContent = name
     this._about.textContent = about
+    this._avatar.src = avatar
   }
 }
