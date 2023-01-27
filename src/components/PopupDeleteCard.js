6f82c3ac4af106bd87d1c72ed802e289caa176db
Ran Pol <rnd809@gmail.com>
2023-01-27 11:47:39 -0500

    Fix error that is caused by the memory leak in the deleting a card process.

--- src/components/PopupDeleteCard.js
+++ src/components/PopupDeleteCard.js
@@ -8,10 +8,19 @@ export default class PopupDeleteCard extends Popup {
 
   open(card) {
     super.open()
+    this._confirmDelete = () => {
+      this._handleCardDelete(card)
+    }
+
     this._popupElement
       .querySelector('.modal__button-yes')
-      .addEventListener('click', () => {
-        this._handleCardDelete(card)
-      })
+      .addEventListener('click', this._confirmDelete)
+  }
+
+  close() {
+    super.close()
+    this._popupElement
+      .querySelector('.modal__button-yes')
+      .removeEventListener('click', this._confirmDelete)
   }
 }
